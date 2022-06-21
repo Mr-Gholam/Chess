f5f06e18992b3db86050464cc8315c463b5ba893
Mr Gholam <mobingh093@gmail.com>
2022-06-21 22:53:27 +0430

    project started

---
+++ main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"image/color"
+	"math/rand"
+	"time"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"github.com/notnil/chess"
+)
+
+func main() {
+	a := app.New()
+	win := a.NewWindow("Chess")
+	win.Resize(fyne.NewSize(800, 800))
+	game := chess.NewGame()
+	grid := createGrid(game.Position().Board())
+	over := canvas.NewImageFromResource(nil)
+	over.Hide()
+	win.SetContent(container.NewMax(grid, container.NewWithoutLayout(over)))
+	go func() {
+		rand.Seed(time.Now().Unix())
+		for game.Outcome() == chess.NoOutcome {
+			time.Sleep(time.Millisecond * 500)
+			valid := game.ValidMoves()
+			m := valid[rand.Intn(len(valid))]
+			move(m, game, grid, over)
+		}
+	}()
+	win.ShowAndRun()
+}
+
+func createGrid(board *chess.Board) *fyne.Container {
+
+	grid := container.NewGridWithColumns(8)
+	for y := 7; y >= 0; y-- {
+		for x := 0; x < 8; x++ {
+			bg := canvas.NewRectangle(color.Gray{0xE0})
+			if x%2 == y%2 {
+				bg.FillColor = color.Gray{0x30}
+			}
+			p := board.Piece(chess.Square(x + y*8))
+			img := canvas.NewImageFromResource(resourceForPiece(p))
+			img.FillMode = canvas.ImageFillContain
+			grid.Add(container.NewMax(bg, img))
+		}
+	}
+	return grid
+}
+
+func move(m *chess.Move, game *chess.Game, grid *fyne.Container, over *canvas.Image) {
+	off := squareToOffset(m.S1())
+	cell := grid.Objects[off].(*fyne.Container)
+	img := cell.Objects[1].(*canvas.Image)
+	pos1 := cell.Position()
+
+	over.Resource = img.Resource
+	over.Move(pos1)
+	over.Resize(img.Size())
+
+	over.Show()
+	img.Resource = nil
+	img.Refresh()
+
+	off = squareToOffset(m.S2())
+	cell = grid.Objects[off].(*fyne.Container)
+	pos2 := cell.Position()
+
+	animation := canvas.NewPositionAnimation(pos1, pos2, time.Millisecond*500, func(p fyne.Position) {
+		over.Move(p)
+		over.Refresh()
+	})
+	animation.Start()
+	time.Sleep(time.Millisecond * 550)
+
+	game.Move(m)
+	refreshGrid(grid, game.Position().Board())
+	over.Hide()
+}
+func refreshGrid(grid *fyne.Container, board *chess.Board) {
+	y, x := 7, 0
+	for _, cell := range grid.Objects {
+		p := board.Piece(chess.Square(x + y*8))
+		img := cell.(*fyne.Container).Objects[1].(*canvas.Image)
+		img.Resource = resourceForPiece(p)
+		img.Refresh()
+
+		x++
+		if x == 8 {
+			x = 0
+			y--
+		}
+	}
+}
+func squareToOffset(sq chess.Square) int {
+	x := sq % 8
+	y := 7 - ((sq - x) / 8)
+	return int(x + y*8)
+}
---
+++ pieces.go
@@ -0,0 +1,44 @@
+//go:generate fyne bundle -o bundled.go pieces
+
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"github.com/notnil/chess"
+)
+
+func resourceForPiece(p chess.Piece) fyne.Resource {
+	switch p.Color() {
+	case chess.Black:
+		switch p.Type() {
+		case chess.Pawn:
+			return resourceBlackPawnSvg
+		case chess.Bishop:
+			return resourceBlackBishopSvg
+		case chess.King:
+			return resourceBlackKingSvg
+		case chess.Knight:
+			return resourceBlackKnightSvg
+		case chess.Queen:
+			return resourceBlackQueenSvg
+		case chess.Rook:
+			return resourceBlackRookSvg
+		}
+	case chess.White:
+		switch p.Type() {
+		case chess.Pawn:
+			return resourceWhitePawnSvg
+		case chess.Bishop:
+			return resourceWhiteBishopSvg
+		case chess.King:
+			return resourceWhiteKingSvg
+		case chess.Knight:
+			return resourceWhiteKnightSvg
+		case chess.Queen:
+			return resourceWhiteQueenSvg
+		case chess.Rook:
+			return resourceWhiteRookSvg
+		}
+	}
+	return nil
+}
